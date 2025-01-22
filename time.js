56af79a784775826dc7fa4a6fee9bfae679e5ad0
Bijaya Limbu <bijusanwa@gmail.com>
2025-01-22 13:40:08 +0545

    Update time.js

--- time.js
+++ time.js
@@ -3,13 +3,18 @@ function updateTime() {
     timeZone: 'Asia/Kathmandu',
     hour12: false, 
     hour: '2-digit',
-    minute: '2-digit',
-    month: 'short', 
-    day: 'numeric', 
   };
 
   const now = new Date().toLocaleString('en-US', options);
+  const currentHour = parseInt(now.substring(0, 2)); 
+
   document.getElementById('time-display').innerText = now;
+
+  if (currentHour >= 18) {
+    document.body.style.backgroundColor = 'black';
+  } else {
+    document.body.style.backgroundColor = 'white'; // or your preferred default color
+  }
 }
 
 // Update time every second
