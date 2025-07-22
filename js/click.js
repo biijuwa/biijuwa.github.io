46bc94b83268df7e76a4d0d3de1f43cf6e1a5dc0
Bijaya Limbu <bijusanwa@gmail.com>
2025-07-22 15:15:44 +0545

    changes

--- js/click.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const redCircle = document.querySelector('.clickable-circle');
-    redCircle.addEventListener('click', () => {
-      alert('Red circle clicked!');
-    });
-  });
-  
